d441a49d746c62c4a211835277f31dfd15bf589b
Van-Giang NGUYEN <van.giang.nguyen@outlook.com.vn>
2023-02-07 14:52:31 +0700

    cau1

--- cau1.js
+++ cau1.js
@@ -1,18 +1,17 @@
-let diemThi = [];
-
-//Nhập mảng
-let N = +prompt("Số lượng điểm thi cần tính là: ");
-for (let i = 0; i < N; i++) {
-    diemThi.push(parseInt(prompt(`Nhập điểm thi thứ ${i+1}`)));
-}
-
-//Duyệt mảng và tính giá trị trung bình
+//Hàm duyệt mảng và tính giá trị trung bình
 function findAvg(arr) {
     let sum = 0;
     arr.forEach(function (value, index, array){
         sum += value;
     })
     return (sum/arr.length).toFixed(1);
+}
+
+//Nhập mảng
+let diemThi = [];
+let N = +prompt("Số lượng điểm thi cần tính là: ");
+for (let i = 0; i < N; i++) {
+    diemThi.push(parseInt(prompt(`Nhập điểm thi thứ ${i+1}`)));
 }
 
 //Trả kết quả
